04e71ea5e0d900747bc6b34c6b14299072b562b4
Zavolzhenskyi Ivan <ven.volga@gmail.com>
2023-08-05 13:18:43 +0300

    Added fonts and login form

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,25 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
+from authentication.forms import LoginForm
 
 
 def login_user(request):
-    return render(request, 'auth/login.html')
+    context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('main_page')
+            else:
+                context = {
+                    'login_form': login_form,
+                    'attention': f'The user with name "{username}" was not found or password incorrect!'
+                }
+    return render(request, 'auth/login.html', context)
 
 
 def register_user(request):
