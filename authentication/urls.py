46d1dc5afab17caa25ae78ec448005fe8aec1e6b
Zavolzhenskyi Ivan <ven.volga@gmail.com>
2023-08-13 10:59:23 +0300

    Reset password logic

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from authentication.views import RegisterUser, UserPage
 
+import django.contrib.auth.urls
+
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('info/', UserPage.as_view(), name='user'),
--- lampovo_shop/urls.py
+++ lampovo_shop/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('user/', include('authentication.urls')),
+    path('users/', include('authentication.urls')),
 ]
